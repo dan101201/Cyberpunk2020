dfca72bc707cd33c95ed8b3eaa88674f3f72df96
Tadayo <daniel.reffeldt@outlook.com>
2019-08-20 13:49:00 +0200

    Added XML implementation to weapons&armor

---
+++ Cyberpunk2020CC/Cyberpunk2020CC/Armor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Cyberpunk2020CharacterCreator.Utility;
+using System.Xml;
+
+namespace Cyberpunk2020CharacterCreator
+{
+    //Gear
+    class Armor
+    {
+        //Name of armor
+        public string name;
+
+        //Stopping Power
+        public int sp = 0;
+
+        //Encumberance
+        public int ev = 0;
+
+        public double cost = 0;
+
+        static Armor[] XmlDocToArmorArray(string path)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(path);
+            List<Armor> armors = new List<Armor>();
+
+            XmlNodeList list = doc.SelectNodes("role");
+            foreach (XmlNode node in list)
+            {
+                Armor tempArmor = new Armor();
+
+                tempArmor.name = XmlRemoveAllChildren(node, "name").InnerText;
+                tempArmor.sp = int.Parse(XmlRemoveAllChildren(node, "sp").InnerText);
+                tempArmor.ev = int.Parse(XmlRemoveAllChildren(node, "ev").InnerText);
+                tempArmor.cost = double.Parse(XmlRemoveAllChildren(node, "cost").InnerText);
+
+                armors.Add(tempArmor);
+            }
+
+            return armors.ToArray();
+        }
+
+    }
+
+    
+
+}
