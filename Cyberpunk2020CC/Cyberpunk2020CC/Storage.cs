c10776fa34a9f4d9a066a6098e82c25158fe24d1
Tadayo <daniel.reffeldt@outlook.com>
2019-08-20 11:45:47 +0200

    Made Storage.cs

---
+++ Cyberpunk2020CC/Cyberpunk2020CC/Storage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyberpunk2020CC
+{
+    //for storing all characters and other useful things, maybe rename to driver
+    class Storage
+    {
+    }
+}
