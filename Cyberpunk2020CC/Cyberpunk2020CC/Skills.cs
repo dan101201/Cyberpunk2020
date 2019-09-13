492c9b13a3085c6f35a182365ff5b3ca08c0fe5b
Tadayo <daniel.reffeldt@outlook.com>
2019-09-13 18:46:37 +0200

    fixed the fixed that wasnt even trying to fix the fix

--- Cyberpunk2020CC/Cyberpunk2020CC/Skills.cs
+++ Cyberpunk2020CC/Cyberpunk2020CC/Skills.cs
@@ -16,245 +16,102 @@ namespace Cyberpunk2020CharacterCreator
     class Skills
     {
         //Dictionary of the skills, with a string key being the nameo f the skill, and int being the level in the skill
-        public static Dictionary<string, int> returnSkillDictionary(bool interlock)
-        {
-            Dictionary<string, int> skills = new Dictionary<string, int>();
-            foreach(string skill in returnSkillArray(interlock))
-            {
-                skills.Add(skill, 0);
-            }
-            return skills;
-        }
-        
-            
+        Dictionary<string, int> skills = new Dictionary<string, int>();
         
 
-    //Returns a string array full of all the skills, using either the interlock(official system) or a homebrew one
-    static string[] returnSkillArray(bool InterLock)
+        public int this[string i]
         {
-            if (!InterLock)
+            get
             {
-                string[] temp =
-        {
-        "PersonalGrooming",
-        "WardrobeAndStyle",
-        "Endurance",
-        "StrengthFeat",
-        "Swimming",
-        "Interrogation",
-        "Intimidate",
-        "Oratory",
-        "ResistTortureOrDrugs",
-        "Streetwise",
-        "HumanPerception",
-        "Interview",
-        "Leadership",
-        "Seduction",
-        "Social",
-        "PersuassionAndFastTalk",
-        "Perform",
-        "Accounting",
-        "Anthropology",
-        "Awareness/Notice",
-        "Biology",
-        "Botany",
-        "Chemisty",
-        "Compossition",
-        "DiagnoseIllness",
-        "EdducationAndGeneralKnowledge",
-        "Expert",
-        "Gamble",
-        "Geology",
-        "HideOrEvade",
-        "History",
-        "LibrarySearch",
-        "Mathematics",
-        "Physics",
-        "Programming",
-        "ShadowOrTrack",
-        "StockMarket",
-        "SystemKnowledge",
-        "Teaching",
-        "WildernessSurvival",
-        "Zoology",
-        "Archery",
-        "Athletics",
-        "Brawling",
-        "Dance",
-        "DodgeAndEscape",
-        "Driving",
-        "Fencing",
-        "Handgun",
-        "HeavyWeapons",
-        "Melee",
-        "Motorcycle",
-        "OperateHeavyMachinery",
-        "PilotGyro",
-        "PilotFixedWing",
-        "PilotDirigible",
-        "PilotVectorThrust",
-        "Rifle",
-        "Stealth",
-        "Submachinegun",
-        "AeroTech",
-        "AVTech",
-        "BasicTech",
-        "CryotankOperation",
-        "CyberdeckDesign",
-        "CyberTech",
-        "Demolitions",
-        "Disguise",
-        "Electronics",
-        "ElectronicSecurity",
-        "FirstAid",
-        "Forgery",
-        "GyroTech",
-        "PaintOrDraw",
-        "PhotoAndFilm",
-        "Pharmacuticals",
-        "PickLock",
-        "PickPocket",
-        "PlayInstrument",
-        "WeaponSmith"
-        };
-                return temp;
+                return skills[i];
             }
-            else
+            set
             {
-
-                string[] temp = {
-"Wardrobe &amp; Style",
-"Personal Grooming",
-"BODY",
-"Endurance",
-"Fitness/Body Building",
-"Rowing",
-"Strength Feat",
-"Swimming",
-"Intimidate",
-"Interrogation",
-"Leadership",
-"Oratory",
-"Resist Torture/Drugs",
-"Skydiving",
-"Streetwise",
-"Animal Handling",
-"Design",
-"Human Perception",
-"Hypnotism/Brainwashing",
-"Interview",
-"Lip Reading",
-"Massage",
-"Networking",
-"Parenting",
-"Perform",
-"Persuasion &amp; Fast Talk",
-"Seduction",
-"Sing",
-"Social",
-"Storytelling",
-"Accounting",
-"Appraise",
-"Awareness: Notice",
-"Awareness: Tactical",
-"Awareness Track",
-"Business Sense",
-"Composition",
-"Diagnose Illness",
-"Education &amp; General Knowledge",
-"Expert: (Subject)",
-"Gamble",
-"Gardening/Farming",
-"Language: (Choose One)",
-"Library Search",
-"Navigation",
-"Programming",
-"S.C.U.B.A.",
-"Survival: (Environment)",
-"System Knowledge",
-"Teaching",
-"Athletics",
-"Blind Fighting",
-"Brawling/Melee",
-"Dance",
-"Initiative",
-"Juggle",
-"Martial Arts: (Style)",
-"Operate: ACPA",
-"Operate: Animal",
-"Operate: Car/Truck",
-"Operate: Dirigible",
-"Operate: EVA",
-"Operate: Fixed Wing",
-"Operate: Glider",
-"Operate: Gyro",
-"Operate: Heavy Machinery",
-"Operate: Motorcycle",
-"Operate: OTV",
-"Operate: Remote",
-"Operate: Sail Driven",
-"Operate: Space Plane/Shuttle",
-"Operate: Sub (Large/Small)",
-"Operate: Vectored Thrust",
-"Skate/Ski/Surf",
-"Stealth/Evasion",
-"Underwater Maneuver",
-"Weapon: Archery",
-"Weapon: Handgun",
-"Weapon: Heavy Weapons",
-"Weapon: Rifle",
-"Zero-G Maneuver",
-"Calligraphy",
-"Cooking",
-"Cryotank Operation",
-"Demolitions",
-"Disguise",
-"Electronic Security",
-"Forgery",
-"Glass blowing",
-"Jeweler",
-"Jury Rig",
-"Med: First Aid",
-"Med: Pharmaceuticals",
-"Med:Surgery",
-"Origami",
-"Photography &amp; Film",
-"Pick Lock",
-"Pick Pocket/Sleight of hand",
-"Play Instrument",
-"Rope Use",
-"Paint or Draw",
-"Sculpt",
-"Sewing",
-"Stage Magic",
-"Tattooing (Hand-Pick)",
-"Traps and snares",
-"Tattooing",
-"Tech: Aero",
-"Tech: AV Tech: Basic",
-"Tech: Carpentry",
-"Tech: Chemistry",
-"Tech: Cyber",
-"Tech: Cyberdeck Design",
-"Tech: Electronics",
-"Tech: Gyro",
-"Tech: Marine/Underwater Equip",
-"Tech: Metal Smith",
-"Tech: Powered Armor",
-"Tech: Pressure Suit",
-"Tech: Spacecraft",
-"Tech: Submarine",
-"Tech: Weaponsmith",
-"Tech: Wetware",
-"Typing",
-"Video Manipulation"
-                    };
-                return temp;
+                skills[i] = value;
             }
-
-
-
         }
+
+        public int PersonalGrooming() { return skills["PersonalGrooming"]; }
+        public int WardrobeAndStyle() { return skills["WardrobeAndStyle"]; }
+        public int Endurance() { return skills["Endurance"]; }
+        public int StrengthFeat() { return skills["StrengthFeat"]; }
+        public int Swimming() { return skills["Swimming"]; }
+        public int Interrogation() { return skills["Interrogation"]; }
+        public int Intimidate() { return skills["Intimidate"]; }
+        public int Oratory() { return skills["Oratory"]; }
+        public int ResistTortureOrDrugs() { return skills["ResistTortureOrDrugs"]; }
+        public int Streetwise() { return skills["Streetwise"]; }
+        public int HumanPerception() { return skills["HumanPerception"]; }
+        public int Interview() { return skills["Interview"]; }
+        public int Leadership() { return skills["Leadership"]; }
+        public int Seduction() { return skills["Seduction"]; }
+        public int Social() { return skills["Social"]; }
+        public int PersuassionAndFastTalk() { return skills["PersuassionAndFastTalk"]; }
+        public int Perform() { return skills["Perform"]; }
+        public int Accounting() { return skills["Accounting"]; }
+        public int Anthropology() { return skills["Anthropology"]; }
+        public int AwarenessNotice() { return skills["AwarenessNotice"]; }
+        public int Biology() { return skills["Biology"]; }
+        public int Botany() { return skills["Botany"]; }
+        public int Chemisty() { return skills["Chemisty"]; }
+        public int Compossition() { return skills["Compossition"]; }
+        public int DiagnoseIllness() { return skills["DiagnoseIllness"]; }
+        public int EdducationAndGeneralKnowledge() { return skills["EdducationAndGeneralKnowledge"]; }
+        public int Expert() { return skills["Expert"]; }
+        public int Gamble() { return skills["Gamble"]; }
+        public int Geology() { return skills["Geology"]; }
+        public int HideOrEvade() { return skills["HideOrEvade"]; }
+        public int History() { return skills["History"]; }
+        public int LibrarySearch() { return skills["LibrarySearch"]; }
+        public int Mathematics() { return skills["Mathematics"]; }
+        public int Physics() { return skills["Physics"]; }
+        public int Programming() { return skills["Programming"]; }
+        public int ShadowOrTrack() { return skills["ShadowOrTrack"]; }
+        public int StockMarket() { return skills["StockMarket"]; }
+        public int SystemKnowledge() { return skills["SystemKnowledge"]; }
+        public int Teaching() { return skills["Teaching"]; }
+        public int WildernessSurvival() { return skills["WildernessSurvival"]; }
+        public int Zoology() { return skills["Zoology"]; }
+        public int Archery() { return skills["Archery"]; }
+        public int Athletics() { return skills["Athletics"]; }
+        public int Brawling() { return skills["Brawling"]; }
+        public int Dance() { return skills["Dance"]; }
+        public int DodgeAndEscape() { return skills["DodgeAndEscape"]; }
+        public int Driving() { return skills["Driving"]; }
+        public int Fencing() { return skills["Fencing"]; }
+        public int Handgun() { return skills["Handgun"]; }
+        public int HeavyWeapons() { return skills["HeavyWeapons"]; }
+        public int Melee() { return skills["Melee"]; }
+        public int Motorcycle() { return skills["Motorcycle"]; }
+        public int OperateHeavyMachinery() { return skills["OperateHeavyMachinery"]; }
+        public int PilotGyro() { return skills["PilotGyro"]; }
+        public int PilotFixedWing() { return skills["PilotFixedWing"]; }
+        public int PilotDirigible() { return skills["PilotDirigible"]; }
+        public int PilotVectorThrust() { return skills["PilotVectorThrust"]; }
+        public int Rifle() { return skills["Rifle"]; }
+        public int Stealth() { return skills["Stealth"]; }
+        public int Submachinegun() { return skills["Submachinegun"]; }
+        public int AeroTech() { return skills["AeroTech"]; }
+        public int AVTech() { return skills["AVTech"]; }
+        public int BasicTech() { return skills["BasicTech"]; }
+        public int CryotankOperation() { return skills["CryotankOperation"]; }
+        public int CyberdeckDesign() { return skills["CyberdeckDesign"]; }
+        public int CyberTech() { return skills["CyberTech"]; }
+        public int Demolitions() { return skills["Demolitions"]; }
+        public int Disguise() { return skills["Disguise"]; }
+        public int Electronics() { return skills["Electronics"]; }
+        public int ElectronicSecurity() { return skills["ElectronicSecurity"]; }
+        public int FirstAid() { return skills["FirstAid"]; }
+        public int Forgery() { return skills["Forgery"]; }
+        public int GyroTech() { return skills["GyroTech"]; }
+        public int PaintOrDraw() { return skills["PaintOrDraw"]; }
+        public int PhotoAndFilm() { return skills["PhotoAndFilm"]; }
+        public int Pharmacuticals() { return skills["Pharmacuticals"]; }
+        public int PickLock() { return skills["PickLock"]; }
+        public int PickPocket() { return skills["PickPocket"]; }
+        public int PlayInstrument() { return skills["PlayInstrument"]; }
+        public int WeaponSmith() { return skills["WeaponSmith"]; }
+
     }
 }
 
